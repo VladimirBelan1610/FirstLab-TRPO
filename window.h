7024fd01924bee3d4d7362d4441a9f431da100f0
VladimirBelan1610 <v.i.belan@viarune.com>
2024-03-13 14:59:19 +0700

    Make around objects

---
+++ window.h
@@ -0,0 +1,19 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "area.h"
+#include <QWidget>
+#include <QtGui>
+#include <QPushButton>
+
+class Window : public QWidget
+{
+protected:
+    QTextCodec *codec;
+    Area * area; // область отображения рисунка
+    QPushButton * btn;
+public:
+    Window();
+};
+
+#endif // WINDOW_H
